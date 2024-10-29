ded7a076fc47ce3ffa916bb6c14637c8c068acd2
Brian Jemilo II <jemiloii@gmail.com>
2024-10-29 01:42:50 -0500

    wildcard

--- dist/angular/server/main.server.mjs
+++ dist/angular/server/main.server.mjs
@@ -1,14 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  main_server_default
-} from "./chunk-IEG6M5EX.mjs";
-import "./chunk-7Y46KNKJ.mjs";
-import "./chunk-NYFG4232.mjs";
-import "./chunk-IM6ALCTB.mjs";
-import "./chunk-N65XVK4U.mjs";
-import "./chunk-4HCVDDTL.mjs";
-import "./chunk-24VIC3GD.mjs";
-export {
-  main_server_default as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-QXLGDUQ5.mjs";import"./chunk-JJQQX3GQ.mjs";import"./chunk-NNXUEOW5.mjs";import"./chunk-WVRZCSHZ.mjs";import"./chunk-PFHV57DE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
