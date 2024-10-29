de7d7c9aeaf278dbaa6767b57b6d891df908b0c1
Brian Jemilo II <jemiloii@gmail.com>
2024-10-29 02:00:08 -0500

    double call stuff for the time being

--- src/e-commerce/views/products/products.view.ts
+++ src/e-commerce/views/products/products.view.ts
@@ -21,7 +21,13 @@ export class ProductsView implements OnInit {
   @Input() breadcrumbs: boolean = true;
   products: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.http.get<any>(`${BACKEND_URL}/api/products`)
+      .subscribe(({ products }) => {
+        console.log('Products:', products);
+        this.products = products;
+      });
+  }
 
   ngOnInit() {
     this.http.get<any>(`${BACKEND_URL}/api/products`)
